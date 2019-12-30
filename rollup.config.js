6b8ae73b7355d51765ee90bda0d2a7ae29efdb9b
chenghao <hao.cheng@karakal.com.cn>
2019-12-31 00:58:05 +0800

    initial dev config

---
+++ rollup.config.js
@@ -0,0 +1,31 @@
+import nodeResolve from 'rollup-plugin-node-resolve';
+import typescriptPlugin from 'rollup-plugin-typescript2';
+import { eslint } from 'rollup-plugin-eslint';
+import pkg from './package.json';
+
+function forUmd() {
+    return {
+        name: pkg.name,
+    };
+}
+function fromSource(format) {
+    return {
+        input: 'src/index.ts',
+        external: ['react'],
+        output: {
+            ...(format === 'umd' && forUmd()),
+            file: `lib/index.${format}.js`,
+            format: format,
+            sourcemap: true,
+        },
+        plugins: [
+            eslint(),
+            nodeResolve({
+                extensions: ['.ts', '.tsx'],
+                mainFields: ['module', 'main']
+            }),
+            typescriptPlugin(),
+        ],
+    };
+}
+export default [...['esm', 'cjs', 'umd'].map(format => fromSource(format))];
